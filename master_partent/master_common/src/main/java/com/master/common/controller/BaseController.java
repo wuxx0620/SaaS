e2d49f0c4a46ced3f8a9d781b69c569614ed2689
春风十里不如你 <1207692854@qq.com>
2020-11-16 10:04:24 +0800

    pom 修改

--- master_partent/master_common/src/main/java/com/master/common/controller/BaseController.java
+++ master_partent/master_common/src/main/java/com/master/common/controller/BaseController.java
@@ -1,44 +1,45 @@
-//package com.master.common.controller;
-//
-//import org.apache.shiro.SecurityUtils;
-//import org.apache.shiro.subject.PrincipalCollection;
-//import org.apache.shiro.subject.Subject;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-///**
-// * @author wuxx
-// * @version 1.0
-// * @date 2020/11/13 17:35
-// * @className BaseController
-// * @description TODO
-// */
-//public class BaseController {
-//    public HttpServletRequest request;
-//    public HttpServletResponse response;
-//    protected String companyId;
-//    protected String companyName;
-//    protected String userId;
-//
-//    //使用shiro获取
-//    //进入控制器之前执行的方法
-//    @ModelAttribute
-//    public void serResAndReq(HttpServletRequest request,HttpServletResponse response){
-//        this.request = request;
-//        this.response = response;
-//
-//        //获取session中的安全数据
-//        Subject subject = SecurityUtils.getSubject();
-//        //subject获取所有的安全集合
-//        PrincipalCollection principals = subject.getPrincipals();
-//        if (principals != null && !principals.isEmpty()){
-//            //获取安全数据
-//            ProfileResult result = (ProfileResult) principals.getPrimaryPrincipal();
-//            this.companyId = result.getCompanyId();
-//            this.companyName = result.getCompany();
-//            this.userId = result.getUserId();
-//        }
-//    }
-//}
+package com.master.common.controller;
+
+import com.master.domain.system.response.ProfileResult;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author wuxx
+ * @version 1.0
+ * @date 2020/11/13 17:35
+ * @className BaseController
+ * @description TODO
+ */
+public class BaseController {
+    public HttpServletRequest request;
+    public HttpServletResponse response;
+    protected String companyId;
+    protected String companyName;
+    protected String userId;
+
+    //使用shiro获取
+    //进入控制器之前执行的方法
+    @ModelAttribute
+    public void serResAndReq(HttpServletRequest request,HttpServletResponse response){
+        this.request = request;
+        this.response = response;
+
+        //获取session中的安全数据
+        Subject subject = SecurityUtils.getSubject();
+        //subject获取所有的安全集合
+        PrincipalCollection principals = subject.getPrincipals();
+        if (principals != null && !principals.isEmpty()){
+            //获取安全数据
+            ProfileResult result = (ProfileResult) principals.getPrimaryPrincipal();
+            this.companyId = result.getCompanyId();
+            this.companyName = result.getCompany();
+            this.userId = result.getUserId();
+        }
+    }
+}
