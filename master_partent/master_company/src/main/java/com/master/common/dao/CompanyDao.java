813ce28a9447ab80da536de9315835b383b018c3
春风十里不如你 <1207692854@qq.com>
2020-11-19 17:56:46 +0800

    git 忽略文件

--- master_partent/master_company/src/main/java/com/master/common/dao/CompanyDao.java
+++
@@ -1,9 +0,0 @@
-package com.master.common.dao;
-
-import com.master.domain.company.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CompanyDao extends JpaRepository<Company, String>, JpaSpecificationExecutor<Company> {
-
-}
